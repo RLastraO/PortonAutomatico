0b2471f4a14752ac47c598e3862da92f6e3c8fb9
RLastraO <raul.lastra.ossandon@gmail.com>
2018-07-16 17:09:46 -0400

    version de prueba, instrucciones eléctricas deshabilitadas, máquina de estado operando.

--- src/main.cpp
+++ src/main.cpp
@@ -10,66 +10,99 @@ const int relay1Input3 = 5; //Pin control relay 3 , power source actuador 1 port
 const int relay1Input4 = 4; //Pin control relay 4 , power source actuador 2 porton.
 int pin_control = 2; //Pin control interrupciones
 
-int estado_pin_control;
-bool portonLow;
-bool portonHigh;
-bool portonChange;
+volatile byte estado_pin_control;
+volatile byte control_state;
+
 int estado_actual;
 int estado_anterior;
+bool first;
 
-void ServicioPortonChange() 
+void controlChange() 
 {
-    portonChange = true;
+    control_state = HIGH;
+    estado_pin_control = digitalRead(pin_control);
+    delay(100);
+}
+
+void parar()
+{
+    //digitalWrite(relay1Input1, LOW);    // Apagamos cerradura (debería estar apagada desde antes, opera con timer).
+    //digitalWrite(relay1Input2, LOW);    // Apagamos baliza
+    /*Se apagan entradas al actuador del porton*/
+    //digitalWrite(relay1Input3, LOW); 
+    //digitalWrite(relay1Input4, LOW);
+    /*******************************************/
+    estado_anterior = estado_actual;
+    estado_actual = DETENIDO;
+}
+
+void actuar()
+{
+    //digitalWrite(relay1Input2, HIGH);   // Baliza, operando mientras opera actuador.
+    //digitalWrite(relay1Input1, HIGH);   // Se opera cerradura a 12 volts.
+    //delay(2000);                        // Se espera 2 segundo para asegurar apertura de cerradura.
+    //digitalWrite(relay1Input1, LOW);    // Se opera cerradura a 12 volts.
+    if(estado_actual == DETENIDO)
+    {
+        switch (estado_anterior)
+        {
+            case ABRIENDO:
+                //digitalWrite(relay1Input3, HIGH);
+                //digitalWrite(relay1Input4, LOW);
+                estado_actual = CERRANDO;
+                break;
+            case CERRANDO:
+                //digitalWrite(relay1Input3, LOW);
+                //digitalWrite(relay1Input4, HIGH);  
+                estado_actual = ABRIENDO;
+                break;
+            default:
+                break;
+        }
+    }
+    else
+    {
+        parar();
+    }  
 }
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
-    attachInterrupt( 0, ServicioPortonChange, CHANGE);
-    portonChange = false;
+    pinMode(pin_control, INPUT_PULLUP);
+    control_state = LOW;
+    attachInterrupt( digitalPinToInterrupt(pin_control), controlChange, CHANGE);
     estado_pin_control = LOW;
     estado_actual = DETENIDO;
+    estado_anterior = CERRANDO;
     pinMode(relay1Input1, OUTPUT);
     pinMode(relay1Input2, OUTPUT);
     pinMode(relay1Input3, OUTPUT);
     pinMode(relay1Input4, OUTPUT);
+    first = true;
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    Serial.println("1");
-    if (portonChange){    
-        Serial.println("CHANGE");
-        estado_pin_control = digitalRead(pin_control);
-        Serial.println(estado_pin_control);
-        delay(1000);
-        portonChange = false;
-    }
-}
+    if (control_state && estado_pin_control==1)
+    { 
+        Serial.println("EJECUTAR ACTUAR");
+        actuar();
+        Serial.print("Anterior  ");
+        Serial.println(estado_anterior);
+        Serial.print("Actual    ");
+        Serial.println(estado_actual);
+        control_state = LOW;
 
-void actuar()
-{
-    digitalWrite(relay1Input2, HIGH); // Baliza, operando mientras opera actuador.
-    digitalWrite(relay1Input1, HIGH); // Se opera cerradura a 12 volts.
-    delay(2000); // Se espera 2 segundo para asegurar apertura de cerradura.
-    
-    switch (estado_anterior)
-    {
-        case ABRIENDO:
-            digitalWrite(relay1Input3, HIGH);
-            break;
-        case CERRANDO:
-            digitalWrite(relay1Input1, HIGH);
-            digitalWrite(relay1Input2, HIGH);  
-            digitalWrite(relay1Input3, HIGH);
-            break;
-        default:
-            break;
-    }
-}
-
-void parar()
-{
-    digitalWrite(relay1Input2, LOW); // Baliza, operando mientras opera actuador.
-    estado_actual = DETENIDO;
+    } 
+    if (control_state && estado_pin_control==0)
+    { 
+        Serial.println("EJECUTAR PARAR");
+        parar();
+        Serial.print("Anterior  ");
+        Serial.println(estado_anterior);
+        Serial.print("Actual    ");
+        Serial.println(estado_actual);
+        control_state = LOW;
+    }     
 }
